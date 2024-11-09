44828a6ba584f8bf47ae68a44e10673660292695
BH_Lin <binghuanlin@gmail.com>
2024-11-09 16:39:56 +0800

    Update JobApp.kt

--- app/src/main/java/com/bh/hackernewsjobsandroid/ui/JobApp.kt
+++ app/src/main/java/com/bh/hackernewsjobsandroid/ui/JobApp.kt
@@ -13,8 +13,6 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Refresh
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.LinearProgressIndicator
@@ -29,7 +27,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.bh.hackernewsjobsandroid.viewmodel.JobViewModel
 
